bcec0098b899dbd2b768cc6bc027a26a02e74ed0
vallesreda <41537468+vallesreda@users.noreply.github.com>
2019-03-24 10:37:22 +0100

    Add files via upload

---
+++ buildCompile3.sh
@@ -0,0 +1,41 @@
+
+##################################################
+#Step2 -- build and compile - first call into pipeline after detecteing some changes into GIT
+# Danith Valles TFG - UCA
+###########################
+#install the angular.
+sudo apt-get update && sudo apt-get -y upgrade
+sudo curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt install git
+sudo npm uninstall angular-cli
+sudo npm install -g @angular/cli
+sudo npm install @angular/cli@latest
+sudo npm install @angular/cli --save-dev
+sudo npm install --save-dev @angular-devkit/build-angular
+
+#Karma installation
+sudo npm install karma --save-dev
+sudo npm install karma-jasmine karma-chrome-launcher jasmine-core --save-dev
+sudo npm install -g karma-cli
+#Jasmine installation
+sudo npm install -g jasmine-core --save-dev
+sudo npm install --save-dev jasmine
+sudo npm install -g jasmine
+sudo npm install rxjs-marbles --save-dev
+sudo npm install jasmine-marbles@0.4.1
+
+# revome old application version
+sudo rm -r -f toh
+# copy from GIT application lastest version
+git clone https://github.com/vallesreda/toh.git
+git clone https://github.com/vallesreda/TFGScript.git
+#Builds and serves your app, rebuilding on file changes.
+# application folder
+#cd toh
+# Compile and start the application at http://localhost:4200/ **
+echo "start NG SERVER"
+sudo sh -c "ng serve &"
+#End of script######
+echo "end script BUILD and COMPILE"
+#exit 0
---
+++ tohTesting.sh
@@ -0,0 +1,9 @@
+##################################################
+#Step3 -- testing toh  - second call into pipeline after compile and build the new version of application
+# Danith Valles TFG - UCA
+###########################
+sudo cd toh
+sudo sh -c "ng test" &
+#End of script TESTING ######
+echo "end script TESTING KARMA and JAZMINE"
+#exit 0
